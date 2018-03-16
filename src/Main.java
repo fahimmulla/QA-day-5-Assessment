4fb07b37328dac94b2eca65c5c3ce32243a799d8
fahimmulla <fahim22@hotmail.co.uk>
2018-03-16 13:38:06 +0000

    new Commits

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
-import friday.Swamp;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		Swamp area = new Swamp();
-		area.grid();
+		Swamp landgrid = new Swamp();
 
+		Player p1 = new Player (4, 1);
+
+		TreasureChest gold = new TreasureChest(7,3);
+		
+		p1.getX();
+		
+		landgrid.grid(p1,gold);
+
+		gold.getX();
 	}
 
 }
---
+++ src/Swamp.java
@@ -0,0 +1,24 @@
+
+public class Swamp {
+
+	public void grid(Player p1, TreasureChest gold) {
+
+		int[][] multiArray = new int[10][10];
+
+		for (int x = 0; x < multiArray.length; x++) {
+			System.out.println();
+			for (int y = 0; y < multiArray.length; y++) {
+				
+					multiArray[p1.getX()][p1.getY()] = 1;
+					
+					multiArray[gold.getX()][gold.getY()] =5;
+					
+				System.out.print(multiArray[x][y]);
+
+		}
+
+	
+	}
+
+}
+}

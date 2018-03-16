08803fe6607826438dca106effa85ccfb142b4ed
fahimmulla <fahim22@hotmail.co.uk>
2018-03-16 11:16:03 +0000

    update commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import friday.Swamp;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Swamp area = new Swamp();
+		area.grid();
+
+	}
+
+}
---
+++ src/friday/Main.java
@@ -0,0 +1,30 @@
+//package friday;
+
+//public class Main {
+
+	/*public static void main(String[] args) {
+
+		Swamp area = new Swamp();
+		area.grid();
+
+	}
+
+	/*
+	 * import java.util.Scanner;
+	 * 
+	 * public class Adventure {
+	 * 
+	 * public static void main(String[] args) {
+	 * 
+	 * Scanner player = new Scanner(System.in);
+	 * 
+	 * 
+	 * /*System.out.println("Input player name"); String name = player.nextLine();
+	 * String begingame = "The Adventure Of the barren moor";
+	 * System.out.println("Type start to begin"); String startgame =
+	 * player.nextLine(); System.out.println("The Adventure of the barren moor");
+	 * System.out.println("------------------------");
+	 * System.out.println("loading");
+	 */
+
+

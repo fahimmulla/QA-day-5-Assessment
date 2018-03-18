959b2cb4e5d359a39722fc95358e6317ac75ef8a
fahimmulla <fahim22@hotmail.co.uk>
2018-03-18 17:51:05 +0000

    tryingcommits

--- src/input.java
+++ src/input.java
@@ -4,7 +4,18 @@ public class input {
 
 	public static Scanner positioninput = new Scanner (System.in);
 	
+	
+	// how to link the scanner into the grid
+	
+	
+	//public static String directionInput (Player p1, int[][] multiArray) {
+		
+	String direction = positioninput.nextLine();
+	//direction//
+
+	}
+	
+//}//
 
 		
-	}
 	

80ebb58101699658403057b4eb89706c6d9e93ab
fahimmulla <fahim22@hotmail.co.uk>
2018-03-19 13:15:45 +0000

    NewWork

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,56 @@
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		
+		{Scanner userinput = new Scanner(System.in);
 
+		System.out.println("Welcome to The Adventure of the barren moor");
+		System.out.println("Lets begin the game");
+		System.out.println("Enter your name");
+	
+		String userName = userinput.nextLine();
+		System.out.println("Welcome :" + userName );
+	
+		System.out.println("You have entered the swamp");
+		
+		System.out.println();
 		Swamp landgrid = new Swamp();
 
-		Player p1 = new Player (4, 1);
+		Player p1 = new Player (1, 1);
 
 		TreasureChest gold = new TreasureChest(7,3);
 		
 		p1.getX();
 		
-	    landgrid.grid(p1,gold);
+	    landgrid.creategrid(p1,gold);
 
 		gold.getX();
+		
+		
+		
+		if (input.equal("north") || input .equals ("south") || input .equals ("east") || input  ("west") {
+		
+		switch (userinput.nextLine().charAt(0))
+		{
+		case ("north"):
+			p1.movetoNorth(landgrid);
+			break;
+		case ("south"):
+			p1.movetoSouth
+			break;
+		case ("east"):
+			p1.movetoEast
+			break;
+		case  ("west"):
+			p1.movetoWest
+			break;
+		default:
+			
 	}
-
-
+	
+		}
+		
+	}
 }
--- src/Player.java
+++ src/Player.java
@@ -2,86 +2,69 @@ public class Player {
 
 	private int x;
 	private int y;
-	
-	
-	
+
 	public Player(int x, int y) {
-	
+
 		this.x = x;
 		this.y = y;
 	}
-
-
 
 	public int getX() {
 		return x;
 	}
 
-
-
 	public void setX(int x) {
 		this.x = x;
 	}
-
-
 
 	public int getY() {
 		return y;
 	}
 
-
-
 	public void setY(int y) {
 		this.y = y;
 	}
 
-
-
-	public void movetoNorth(int [] [] grid) {
-		grid [x][y] = 0;
-		y = y-1;
-		if ( y == -1 ) {
-			y = y +1;
-			return ;
+	public void movetoNorth(int[][] grid) {
+		grid[x][y] = 0;
+		y = y - 1;
+		if (y == -1) {
+			y = y - 1;
+			return;
 		}
-		grid [x][y] = 1;
+		grid[x][y] = -1;
 	}
-		
-		public void movetoSouth(int [] [] grid) {
-			grid [x][y] = 0;
-			y = y+1;
-			if ( y == +1 ) {
-				y = y -1;
-				return ;
-			}
-			grid [x][y] = 1;
+
+	public void movetoSouth(int[][] grid) {
+		grid[x][y] = 0;
+		y = y + 1;
+		if (y == +1) {
+			y = y + 1;
+			return;
 		}
-		
-		public void movetoEast(int [] [] grid) {
-			grid [x][y] = 0;
-			x = x+1;
-			if ( x == +1 ) {
-				x = x -1;
-				return ;
-			}
-			grid [x][y] = 1;
-		
+		grid[x][y] = +1;
+	}
+
+	public void movetoEast(int[][] grid) {
+		grid[x][y] = 0;
+		x = x + 1;
+		if (x == +1) {
+			x = x + 1;
+			return;
 		}
-			public void movetoWest(int [] [] grid) {
-				grid [x][y] = 0;
-				x = x-1;
-				if ( x == -1 ) {
-					x = x +1;
-					return ;
-				}
-				grid [x][y] = 1;
-			
-		// if(p1.getY() == 0) return false, if the statement is not true, then the player is not at the top of the grid
-				//int temp = multiArray[p1.getX()] [p1.getY - 1];
-				//multiArray[p1.getX()][p1.getY() - 1] = 1
-				//multiArray[p1.getX()][p1.getY()] = temp;
-				//p1.setY = p1.getY - 1;
-		
-			}
+		grid[x][y] = +1;
+
+	}
+
+	public void movetoWest(int[][] grid) {
+		grid[x][y] = 0;
+		x = x - 1;
+		if (x == -1) {
+			x = x - 1;
+			return;
+		}
+		grid[x][y] = -1;
+
+	}
 
 }
